b51eada177b9c4cd0d71511210f160a48dcce13e
fahmula <jdazzle44@gmail.com>
2023-10-22 14:39:08 -0400

    Add function to track the last 100 notified items to reduce duplicate notifications

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from requests.exceptions import HTTPError
 from flask import Flask, request
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -24,6 +23,9 @@ rotating_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %
 
 # Add the rotating handler to the logger
 logging.getLogger().addHandler(rotating_handler)
+
+# Store notified items in a dictionary
+notified_items = {}
 
 # Constants
 TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
@@ -95,112 +97,145 @@ def get_youtube_trailer_url(query):
     return f"https://www.youtube.com/watch?v={video_id}" if video_id else "Video not found!"
 
 
+def item_already_notified(item_type, item_name, release_year):
+    key = f"{item_type}:{item_name}:{release_year}"
+    return key in notified_items
+
+
+def mark_item_as_notified(item_type, item_name, release_year, max_entries=100):
+    key = f"{item_type}:{item_name}:{release_year}"
+    notified_items[key] = True
+
+    # Check if the number of entries in notified_items exceeds the limit
+    if len(notified_items) > max_entries:
+        # Get a list of keys (notification identifiers) sorted by their insertion order (oldest first)
+        keys_sorted_by_insertion_order = sorted(notified_items, key=notified_items.get)
+
+        # Remove the oldest entry from the dictionary
+        oldest_key = keys_sorted_by_insertion_order[0]
+        del notified_items[oldest_key]
+        logging.info(f"Key '{oldest_key}' has been deleted from notified_items")
+
+
 @app.route("/webhook", methods=["POST"])
 def announce_new_releases_from_jellyfin():
     try:
         payload = json.loads(request.data)
+        item_type = payload.get("ItemType")
+        item_name = payload.get("Name")
+        release_year = payload.get("Year")
+        series_name = payload.get("SeriesName")
+        season_epi = payload.get("EpisodeNumber00")
+        season_num = payload.get("SeasonNumber00")
 
-        if payload.get("ItemType") == "Movie":
+        if item_type == "Movie":
+            if not item_already_notified(item_type, item_name, release_year):
+                movie_id = payload.get("ItemId")
+                overview = payload.get("Overview")
+                runtime = payload.get("RunTime")
+                # Remove release_year from movie_name if present
+                movie_name = item_name
+                movie_name_cleaned = movie_name.replace(f" ({release_year})", "").strip()
 
-            movie_name = payload.get("Name")
-            movie_id = payload.get("ItemId")
-            release_year = payload.get("Year")
-            overview = payload.get("Overview")
-            runtime = payload.get("RunTime")
-            # Remove release_year from movie_name if present
-            movie_name_cleaned = movie_name.replace(f" ({release_year})", "").strip()
-
-            trailer_url = get_youtube_trailer_url(f"{movie_name_cleaned} Trailer {release_year}")
-
-            notification_message = (
-                f"*🍿New Movie Added🍿*\n\n*{movie_name_cleaned}* *({release_year})*\n\n{overview}\n\n"
-                f"Runtime\n{runtime}")
-
-            if trailer_url:
-                notification_message += f"\n\n[🎥]({trailer_url})[Trailer]({trailer_url})"
-
-            send_telegram_photo(movie_id, notification_message)
-            logging.info(f"(Movie) {movie_name} {release_year} "
-                         f"notification was sent to telegram.")
-            return "Movie notification was sent to telegram"
-
-        if payload.get("ItemType") == "Season":
-
-            series_name = payload.get("SeriesName")
-            season_id = payload.get("ItemId")
-            season = payload.get("Name")
-            release_year = payload.get("Year")
-            season_details = get_item_details(season_id)
-            series_id = season_details["Items"][0].get("SeriesId")
-            series_details = get_item_details(series_id)
-            # Remove release_year from series_name if present
-            series_name_cleaned = series_name.replace(f" ({release_year})", "").strip()
-
-            # Get series overview if season overview is empty
-            overview_to_use = payload.get("Overview") if payload.get("Overview") else series_details["Items"][0].get(
-                "Overview")
-
-            notification_message = (
-                f"*New Season Added*\n\n*{series_name_cleaned}* *({release_year})*\n\n"
-                f"*{season}*\n\n{overview_to_use}\n\n")
-
-            response = send_telegram_photo(season_id, notification_message)
-
-            if response.status_code == 200:
-                logging.info(f"(Season) {series_name_cleaned} {season} "
-                             f"notification was sent to telegram.")
-                return "Season notification was sent to telegram"
-            else:
-                send_telegram_photo(series_id, notification_message)
-                logging.warning(f"{series_name_cleaned} {season} image does not exists, falling back to series image")
-                logging.info(f"(Season) {series_name_cleaned} {season} notification was sent to telegram")
-                return "Season notification was sent to telegram"
-
-        if payload.get("ItemType") == "Episode":
-
-            item_id = payload.get("ItemId")
-            file_details = get_item_details(item_id)
-            season_id = file_details["Items"][0].get("SeasonId")
-            episode_premiere_date = file_details["Items"][0].get("PremiereDate", "0000-00-00T").split("T")[0]
-            season_details = get_item_details(season_id)
-            series_id = season_details["Items"][0].get("SeriesId")
-            season_date_created = season_details["Items"][0].get("DateCreated", "0000-00-00T").split("T")[0]
-            item_name = payload.get("SeriesName")
-            epi_name = payload.get("Name")
-            season_num = payload.get("SeasonNumber00")
-            season_epi = payload.get("EpisodeNumber00")
-            overview = payload.get("Overview")
-
-            if not is_not_within_last_x_days(season_date_created, SEASON_ADDED_WITHIN_X_DAYS):
-                logging.info(f"(Episode) {item_name} Season {season_num} "
-                             f"was added within the last 3 days. Not sending notification.")
-                return "Season was added within the last 3 days. Not sending notification."
-
-            if episode_premiere_date and is_within_last_x_days(episode_premiere_date,
-                                                               EPISODE_PREMIERED_WITHIN_X_DAYS):
+                trailer_url = get_youtube_trailer_url(f"{movie_name_cleaned} Trailer {release_year}")
 
                 notification_message = (
-                    f"*New Episode Added*\n\n*Release Date*: {episode_premiere_date}\n\n*Series*: {item_name} *S*"
-                    f"{season_num}*E*{season_epi}\n*Episode Title*: {epi_name}\n\n{overview}\n\n"
-                )
+                    f"*🍿New Movie Added🍿*\n\n*{movie_name_cleaned}* *({release_year})*\n\n{overview}\n\n"
+                    f"Runtime\n{runtime}")
+
+                if trailer_url:
+                    notification_message += f"\n\n[🎥]({trailer_url})[Trailer]({trailer_url})"
+
+                send_telegram_photo(movie_id, notification_message)
+                mark_item_as_notified(item_type, item_name, release_year)
+                logging.info(f"(Movie) {movie_name} {release_year} "
+                             f"notification was sent to telegram.")
+                return "Movie notification was sent to telegram"
+
+        if item_type == "Season":
+            if not item_already_notified(item_type, item_name, release_year):
+                season_id = payload.get("ItemId")
+                season = item_name
+                season_details = get_item_details(season_id)
+                series_id = season_details["Items"][0].get("SeriesId")
+                series_details = get_item_details(series_id)
+                # Remove release_year from series_name if present
+                series_name_cleaned = series_name.replace(f" ({release_year})", "").strip()
+
+                # Get series overview if season overview is empty
+                overview_to_use = payload.get("Overview") if payload.get("Overview") else series_details["Items"][0].get(
+                    "Overview")
+
+                notification_message = (
+                    f"*New Season Added*\n\n*{series_name_cleaned}* *({release_year})*\n\n"
+                    f"*{season}*\n\n{overview_to_use}\n\n")
+
                 response = send_telegram_photo(season_id, notification_message)
 
                 if response.status_code == 200:
-                    logging.info(f"(Episode) {item_name} S{season_num}E{season_epi} notification sent to Telegram!")
-                    return "Notification sent to Telegram!"
+                    mark_item_as_notified(item_type, item_name, release_year)
+                    logging.info(f"(Season) {series_name_cleaned} {season} "
+                                 f"notification was sent to telegram.")
+                    return "Season notification was sent to telegram"
                 else:
                     send_telegram_photo(series_id, notification_message)
-                    logging.warning(f"(Episode) {item_name} season image does not exists, falling back to series image")
-                    logging.info(f"(Episode) {item_name} S{season_num}E{season_epi} notification sent to Telegram!")
-                    return "Notification sent to Telegram!"
+                    logging.warning(f"{series_name_cleaned} {season} image does not exists, falling back to series image")
+                    logging.info(f"(Season) {series_name_cleaned} {season} notification was sent to telegram")
+                    return "Season notification was sent to telegram"
 
-            else:
-                logging.info(f"(Episode) {item_name} S{season_num}E{season_epi} "
-                             f"was premiered more than {EPISODE_PREMIERED_WITHIN_X_DAYS} "
-                             f"days ago. Not sending notification.")
-                return (f"Episode was added more than {EPISODE_PREMIERED_WITHIN_X_DAYS} "
-                        f"days ago. Not sending notification.")
-        logging.error('Item Type Not Supported')
+        if item_type == "Episode":
+            if not item_already_notified(item_type, item_name, release_year):
+                item_id = payload.get("ItemId")
+                file_details = get_item_details(item_id)
+                season_id = file_details["Items"][0].get("SeasonId")
+                episode_premiere_date = file_details["Items"][0].get("PremiereDate", "0000-00-00T").split("T")[0]
+                season_details = get_item_details(season_id)
+                series_id = season_details["Items"][0].get("SeriesId")
+                season_date_created = season_details["Items"][0].get("DateCreated", "0000-00-00T").split("T")[0]
+                epi_name = item_name
+                overview = payload.get("Overview")
+
+                if not is_not_within_last_x_days(season_date_created, SEASON_ADDED_WITHIN_X_DAYS):
+                    logging.info(f"(Episode) {series_name} Season {season_num} "
+                                 f"was added within the last 3 days. Not sending notification.")
+                    return "Season was added within the last 3 days. Not sending notification."
+
+                if episode_premiere_date and is_within_last_x_days(episode_premiere_date,
+                                                                   EPISODE_PREMIERED_WITHIN_X_DAYS):
+
+                    notification_message = (
+                        f"*New Episode Added*\n\n*Release Date*: {episode_premiere_date}\n\n*Series*: {series_name} *S*"
+                        f"{season_num}*E*{season_epi}\n*Episode Title*: {epi_name}\n\n{overview}\n\n"
+                    )
+                    response = send_telegram_photo(season_id, notification_message)
+
+                    if response.status_code == 200:
+                        mark_item_as_notified(item_type, item_name, release_year)
+                        logging.info(f"(Episode) {series_name} S{season_num}E{season_epi} notification sent to Telegram!")
+                        return "Notification sent to Telegram!"
+                    else:
+                        send_telegram_photo(series_id, notification_message)
+                        logging.warning(f"(Episode) {series_name} season image does not exists, "
+                                        f"falling back to series image")
+                        mark_item_as_notified(item_type, item_name, release_year)
+                        logging.info(f"(Episode) {series_name} S{season_num}E{season_epi} notification sent to Telegram!")
+                        return "Notification sent to Telegram!"
+
+                else:
+                    logging.info(f"(Episode) {series_name} S{season_num}E{season_epi} "
+                                 f"was premiered more than {EPISODE_PREMIERED_WITHIN_X_DAYS} "
+                                 f"days ago. Not sending notification.")
+                    return (f"Episode was added more than {EPISODE_PREMIERED_WITHIN_X_DAYS} "
+                            f"days ago. Not sending notification.")
+
+        if item_type == "Movie":
+            logging.info(f"(Movie) {item_name} Notification Was Already Sent")
+        elif item_type == "Season":
+            logging.info(f"(Season) {series_name} {item_name} Notification Was Already Sent")
+        elif item_type == "Episode":
+            logging.info(f"(Episode) {series_name} S{season_num}E{season_epi} Notification Was Already Sent")
+        else:
+            logging.error('Item type not supported')
         return "Item type not supported."
 
     # Handle specific HTTP errors
